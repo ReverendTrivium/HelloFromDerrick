4ec4e7b0d101c069d85e83a574e0f60ee17b9ac1
Derrick Eberlein <91962608+ReverendTrivium@users.noreply.github.com>
2021-10-06 09:22:47 -0400

    Initial commit

---
+++ src/eberlein/Main.java
@@ -0,0 +1,12 @@
+package eberlein;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Add Name to Code //
+        String name = "Derrick Eberlein";
+
+        // Output Hello with Name included in correct format //
+        System.out.println("Hello, my name is " + name + ".");
+    }
+}
